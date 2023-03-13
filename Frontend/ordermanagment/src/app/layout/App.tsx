aa5f4ed5e05fdeb4810e7536b056f84822135f60
rbasehewa <rbasehewa@gmail.com>
2023-03-13 22:28:42 +1100

    addding up the layout on react

--- Frontend/ordermanagment/src/app/layout/App.tsx
+++ Frontend/ordermanagment/src/app/layout/App.tsx
@@ -1,19 +1,30 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CustomersDashboard from '../../features/customers/customersDashboard/CustomersDashboard';
+import HomePage from '../../features/home/HomePage';
+import Layout from './Layout';
 import './styles.css';
 
 const client  = new ApolloClient({
   cache: new InMemoryCache({
     typePolicies: {}
   }),
-  uri: "http://localhost:5223/graphql/"
-})
+  uri: process.env.REACT_APP_API_SCHEMA_URL
+});
 
 function App() {
   return (
 
     <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage /> } />
+            <Route path="customers" element={<CustomersDashboard /> } />
+          </Route>
+        </Routes>
+      </BrowserRouter>
       <CustomersDashboard />
     </ApolloProvider>
   );
